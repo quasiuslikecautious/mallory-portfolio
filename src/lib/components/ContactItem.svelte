67314bbf52a262e3824d2353457f2a4591782c2e
quasiuslikecautious <zquasius@gmail.com>
2023-07-24 21:25:16 -0500

    Fix location contact handling

--- src/lib/components/ContactItem.svelte
+++ src/lib/components/ContactItem.svelte
@@ -9,38 +9,71 @@
   import Fa from 'svelte-fa'
 </script>
 
-<a
-  href={link == '' ? 'javascript:void(0)' : link}
-  class="
-    flex flex-row space-x-4
-    container shadow rounded-lg {color}
-    mr-8 p-2
-
-    dark:bg-transparent dark:border dark:border-solid dark:border-white
-
-    {link == '' ? "cursor-default" : ''}
-  "
->
-  <div class="
-    w-8 h-8 p-2 ml-2
-  ">
-    <Fa icon={faIcon} primaryColor={faIconColor} size="lg" />
-  </div>
-
-  <div class="
-    flex flex-col justify-center items-start
-  ">
-    <h3 class="
-      text-xs text-text
+{#if link == ''}
+  <div
+    class="
+      flex flex-row space-x-4
+      container shadow rounded-lg {color}
+      mr-8 p-2
+  
+      dark:bg-transparent dark:border dark:border-solid dark:border-white
+    "
+  >
+    <div class="
+      w-8 h-8 p-2 ml-2
     ">
-      {name}
-    </h3>
-    <p
-      class="
-        text-text whitespace-nowrap {link != '' ? "underline" : ""}
-      "
-    >
-      {value}
-    </p>
+      <Fa icon={faIcon} primaryColor={faIconColor} size="lg" />
+    </div>
+  
+    <div class="
+      flex flex-col justify-center items-start
+    ">
+      <h3 class="
+        text-xs text-text
+      ">
+        {name}
+      </h3>
+      <p
+        class="
+          text-text whitespace-nowrap
+        "
+      >
+        {value}
+      </p>
+    </div>
   </div>
-</a>
+{:else}
+  <a
+    href={link}
+    class="
+      flex flex-row space-x-4
+      container shadow rounded-lg {color}
+      mr-8 p-2
+  
+      dark:bg-transparent dark:border dark:border-solid dark:border-white
+    "
+  >
+    <div class="
+      w-8 h-8 p-2 ml-2
+    ">
+      <Fa icon={faIcon} primaryColor={faIconColor} size="lg" />
+    </div>
+  
+    <div class="
+      flex flex-col justify-center items-start
+    ">
+      <h3 class="
+        text-xs text-text
+      ">
+        {name}
+      </h3>
+      <p
+        class="
+          text-text whitespace-nowrap underline
+        "
+      >
+        {value}
+      </p>
+    </div>
+  </a>
+{/if}
