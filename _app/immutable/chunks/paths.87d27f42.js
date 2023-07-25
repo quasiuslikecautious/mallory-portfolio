946ce11e4194a3acd50ee0d86e3bad37cc832746
quasiuslikecautious <zquasius@gmail.com>
2023-07-24 21:29:22 -0500

    fix production base path

--- _app/immutable/chunks/paths.87d27f42.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1bn3m4e)==null?void 0:s.base)??"/sveltekit-github-pages";var e;const a=((e=globalThis.__sveltekit_1bn3m4e)==null?void 0:e.assets)??t;export{a,t as b};
