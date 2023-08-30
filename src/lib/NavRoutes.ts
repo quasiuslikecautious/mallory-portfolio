f5998ead13fe6a64c7821efa5cdf4f34753d22c0
Zach Quasius <zquasius@gmail.com>
2023-08-30 17:37:19 -0500

    Remove social tab for now

--- src/lib/NavRoutes.ts
+++ src/lib/NavRoutes.ts
@@ -1,4 +1,4 @@
-import { faTiktok } from '@fortawesome/free-brands-svg-icons';
+// import { faTiktok } from '@fortawesome/free-brands-svg-icons';
 import { faFileLines, faUser } from '@fortawesome/free-regular-svg-icons';
 import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
 
@@ -6,7 +6,6 @@ const routes = [
   { section: "about", faIcon: faUser }, 
   { section: "resume", faIcon: faFileLines }, 
   { section: "portfolio", faIcon: faBriefcase }, 
-  { section: "socials", faIcon: faTiktok } 
 ];
 
 export default routes;
